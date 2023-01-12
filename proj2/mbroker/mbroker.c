60af86b93fbf1e3e30a8cfe0a9d42fbe8728204b
CyFireEx <andrecorreialagos@gmail.com>
2023-01-12 00:18:53 +0000

    no signals?

--- proj2/mbroker/mbroker.c
+++ proj2/mbroker/mbroker.c
@@ -1,5 +1,8 @@
 #include "logging.h"
 
+#include "logging.h"
+#include "operations.h"
+
 #include <stdint.h>
 #include <fcntl.h>
 #include <stdbool.h>
@@ -13,12 +16,39 @@
 
 #include <pthread.h>
 
+void publisher(char* clientPipe,char* boxName){
+    int f = tfs_open(boxName,TFS_O_APPEND);
+    if (f == -1){
+        printf("error to do in L126 - mbroker.c\n");
+        return;
+    }
+    char msg[1024];
+    int receivingPipe = open(clientPipe, O_RDONLY);
+    while (true)
+    {    
+        ssize_t readBytes = read(receivingPipe, msg, sizeof(msg));
+        if (readBytes == -1)
+        {
+            printf("error to do 30 mbroker\n");
+        }
+        
+        close(receivingPipe);
+        tfs_write(f, msg, sizeof(msg));
+        if (strlen(msg) == sizeof(msg))
+            printf("no /o here\n");
+        printf("wrote: %s\n",msg);
+    }
+    tfs_close(f);
+}
+
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
     if (argc != 3){
         fprintf(stderr, "usage: mbroker <pipename> <max_sesions>\n");
     }
+
+    tfs_init(NULL);
 
     //long threadCount = (long)argv[2];
 
@@ -33,12 +63,16 @@ int main(int argc, char **argv) {
         fprintf(stderr, "[ERR]: mkfifo failed: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
-    printf("checkpoint 1\n");
-    signal(SIGPIPE, SIG_IGN);
 
-    int receivingPipe = open(argv[1], O_RDONLY);
-    printf("checkpoint 1\n");
+    int receivingPipe = open(argv[1], O_RDWR);
     while (true) {
+
+        signal(SIGPIPE, SIG_IGN);
+
+        int sig;
+        sigwait(&set, &sig);
+        if(sig == SIGUSR1)
+
         char buffer[1024];
         ssize_t readBytes = read(receivingPipe, buffer, sizeof(buffer));
         if (readBytes == -1) {
@@ -48,10 +82,9 @@ int main(int argc, char **argv) {
                 printf("read failed with error: %s\n", strerror(errno));
             break;
         }
-        if (readBytes == 0) // if pipe is closed by other end
-            break;
-        buffer[readBytes] = '\0'; // null terminate the buffer
-                                                         
+        //if (readBytes == 0) // if pipe is closed by other end
+        //    break;
+        //buffer[readBytes] = '\0'; // null terminate the buffer                                                   
         int code = 0;
         sscanf(buffer, "code = %d|%s",&code , buffer);
         char clientPipe[256];
@@ -59,8 +92,8 @@ int main(int argc, char **argv) {
         switch (code)
         {
             case 1:
-                sscanf(buffer, "%s|%s",clientPipe , boxName);
-                //publisher()
+                sscanf(buffer, "%[^|]|%s",clientPipe , boxName);
+                publisher(clientPipe,boxName);
                 break;
 
             case 2:
@@ -69,15 +102,31 @@ int main(int argc, char **argv) {
                 break;
 
             case 3:
-                sscanf(buffer, "%s|%s",clientPipe , boxName);
+                sscanf(buffer, "%[^|]|%s",clientPipe , boxName);
+                int f = tfs_open(boxName, TFS_O_CREAT);
+                if (f == -1){
+                    printf("error to do 102 mbroke\n");
+                }
                 printf("ayo Boxed\n");
-                //create_box()
+                fprintf(stdout, "OK\n");
+                tfs_close(f);
+                int managerPipe = open(clientPipe, O_WRONLY);
+                char sendBuffer[1024];
+                sprintf(sendBuffer, "code = 4|%d|%s", 0, "failed");                                 //this is not right
+                ssize_t w = write(managerPipe, sendBuffer, 1024);
+                if (w < 0) {                                                                      //maybe remove
+                    fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
+
+                close(managerPipe);
                 break;
             
             case 5:
-                sscanf(buffer, "%s|%s",clientPipe , boxName);
+                sscanf(buffer, "%[^|]|%s",clientPipe , boxName);
+                tfs_unlink(boxName);
                 printf("ayo unBoxed\n");
-                //remove_box()
+                fprintf(stdout, "OK\n");
                 break;
 
             case 7:
@@ -88,15 +137,8 @@ int main(int argc, char **argv) {
             default:
                 break;
         }
-
-        printf("%s\n",buffer);
-
-
         
     }
-
-
-
 /*    pthread_t tid[threadCount];
     
     for (int i = 0; i < threadCount; i++)
